768e5d2b9098cd8614c3587fb33d78b822db16ed
Gajesh Naik <robogajesh@gmail.com>
2023-04-06 19:30:33 +0530

    fixed

--- contracts/Destination.sol
+++ contracts/Destination.sol
@@ -8,21 +8,29 @@ import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IUniversalRouter.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./interfaces/IStargateEthVault.sol";
 
 contract Destination is NonblockingLzApp {
     IStargateRouter public stargateRouter;
     IUniversalRouter public universalRouter;
+    IStargateEthVault public stargateEthVault;
 
     /// @param _lzEndpoint - LayerZero Endpoint
     /// @param _stargateRouter - Stargate Router
     /// @param _universalRouter - Universal Router (A.K.A Uniswap NFT Aggregator)
+    /// @param _stargateEthVault - Stargate Eth Vault
     constructor(
         address _lzEndpoint,
         address _stargateRouter,
-        address _universalRouter 
+        address _universalRouter,
+        address _stargateEthVault
     ) NonblockingLzApp(_lzEndpoint) {
+        require(_stargateEthVault != address(0x0), "RouterETH: _stargateEthVault cant be 0x0");
+        require(_stargateRouter != address(0x0), "RouterETH: _stargateRouter cant be 0x0");
+
         stargateRouter = IStargateRouter(_stargateRouter);
         universalRouter = IUniversalRouter(_universalRouter);
+        stargateEthVault = IStargateEthVault(_stargateEthVault);
     }
 
     function executeTrade(
@@ -30,13 +38,13 @@ contract Destination is NonblockingLzApp {
         bytes calldata commands, 
         bytes[] calldata inputs, 
         address _nft,
-				uint256 _tokenId
+        uint256 _tokenId
     ) public payable {
-        try universalRouter.execute{value: msg.value}(commands, inputs, block.timestamp) {
+        try universalRouter.execute{value: _amountLD}(commands, inputs, block.timestamp) {
             // Transfer NFT to sender
             IERC721(_nft).safeTransferFrom(address(this), sender, _tokenId);
         } catch {
-            payable(sender).transfer(msg.value);
+            payable(sender).transfer(_amountLD);
         }
     }
 
@@ -56,17 +64,21 @@ contract Destination is NonblockingLzApp {
         address _token, 
         uint amountLD, 
         bytes memory _payload
-    ) external payable {
+    ) external {
         require(msg.sender == address(stargateRouter), "Unauthorized");
-        require(msg.value > 0, "msg.value must be greater than 0");
+        require(_amountLD > 0, "_amount must be greater than 0");
 
         (
             address sender, 
             bytes memory commands, 
-            bytes[] memory inputs, 
+            bytes[] memory inputs,
             address _nft,
-	          uint256 _tokenId
+            uint256 _tokenId
         ) = abi.decode(_payload, (address, bytes, bytes[], address, uint256));
+
+        // Unwrap & Approve ETH
+        stargateEthVault.approve(address(stargateEthVault), _amountLD);
+        stargateEthVault.withdraw(_amountLD);
 
         this.executeTrade(sender, commands, inputs, _nft, _tokenId);
     }    
